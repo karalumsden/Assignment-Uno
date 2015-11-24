eeb837dabe4c05c2732dd71b07666df22cae0ddc
Lumsdens <Lumsdens@192.168.2.9>
2015-11-23 19:40:13 -0500

    assignment 3

--- src/java/toba/data/DBUtil.java
+++ src/java/toba/data/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 public class DBUtil {
     private static final EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("TobaPU");
+            Persistence.createEntityManagerFactory("TOBAPU");
     
     public static EntityManagerFactory getEmFactory() {
         return emf;
